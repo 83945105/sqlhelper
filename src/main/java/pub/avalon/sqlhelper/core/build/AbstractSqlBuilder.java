7662736783a20a153585b2d27f668440cabb4949
18606167809@163.com <BAI_chao910405>
2018-08-27 21:30:39 +0800

    开发中

--- src/main/java/pub/avalon/sqlhelper/core/build/AbstractSqlBuilder.java
+++ src/main/java/pub/avalon/sqlhelper/core/build/AbstractSqlBuilder.java
@@ -1,5 +1,8 @@
 package pub.avalon.sqlhelper.core.build;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.fusesource.jansi.Ansi;
 import pub.avalon.sqlhelper.core.sql.SqlSplicer;
 
 import java.util.*;
@@ -10,17 +13,31 @@ import java.util.*;
  */
 public abstract class AbstractSqlBuilder implements SqlBuilder {
 
+    private final Log logger = LogFactory.getLog(getClass());
+
     protected SqlSplicer sqlSplicer = new SqlSplicer(128);
 
     protected List<Object> sqlArgs;
 
     @Override
     public String getPreparedStatementSql() {
+        if (this.logger.isDebugEnabled()) {
+            logger.debug(Ansi.ansi().eraseScreen()
+                    .fg(Ansi.Color.YELLOW)
+                    .a("GET PreparedStatementSQL [" + this.sqlSplicer.getSql() + "]")
+                    .reset());
+        }
         return this.sqlSplicer.getSql();
     }
 
     @Override
     public List<Object> getPreparedStatementArgs() {
+        if (this.logger.isDebugEnabled()) {
+            logger.debug(Ansi.ansi().eraseScreen()
+                    .fg(Ansi.Color.RED)
+                    .a("GET PreparedStatementArgs " + this.sqlArgs.toString())
+                    .reset());
+        }
         return this.sqlArgs;
     }
 
