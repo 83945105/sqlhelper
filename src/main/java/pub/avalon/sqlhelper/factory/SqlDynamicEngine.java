0f32a9b50ae519e6d68eb8a627f0794e57d1a257
wx_83945105 <BAI_chao910405>
2019-02-22 14:10:10 +0800

    新增SqlDynamicEngine

---
+++ src/main/java/pub/avalon/sqlhelper/factory/SqlDynamicEngine.java
@@ -0,0 +1,112 @@
+package pub.avalon.sqlhelper.factory;
+
+import pub.avalon.beans.DataBaseType;
+import pub.avalon.sqlhelper.core.beans.*;
+import pub.avalon.sqlhelper.core.engine.*;
+import pub.avalon.sqlhelper.core.norm.Model;
+
+/**
+ * Sql动态引擎
+ *
+ * @author 白超
+ * @date 2019/2/22
+ */
+public class SqlDynamicEngine {
+
+    private DataBaseType dataBaseType;
+
+    public SqlDynamicEngine(DataBaseType dataBaseType) {
+        this.dataBaseType = dataBaseType;
+    }
+
+    public <T extends Model<T, TL, TO, TC, TS, TG>,
+            TL extends ColumnModel<T, TL, TO, TC, TS, TG>,
+            TO extends OnModel<T, TL, TO, TC, TS, TG>,
+            TC extends WhereModel<T, TL, TO, TC, TS, TG>,
+            TS extends SortModel<T, TL, TO, TC, TS, TG>,
+            TG extends GroupModel<T, TL, TO, TC, TS, TG>> TableEngine<T, TL, TO, TC, TS, TG> table(String tableName, Class<T> mainClass) {
+        return new TableEngine<>(tableName, mainClass, dataBaseType);
+    }
+
+    public <T extends Model<T, TL, TO, TC, TS, TG>,
+            TL extends ColumnModel<T, TL, TO, TC, TS, TG>,
+            TO extends OnModel<T, TL, TO, TC, TS, TG>,
+            TC extends WhereModel<T, TL, TO, TC, TS, TG>,
+            TS extends SortModel<T, TL, TO, TC, TS, TG>,
+            TG extends GroupModel<T, TL, TO, TC, TS, TG>> TableEngine<T, TL, TO, TC, TS, TG> table(Class<T> mainClass) {
+        return table(null, mainClass);
+    }
+
+    public <T extends Model<T, TL, TO, TC, TS, TG>,
+            TL extends ColumnModel<T, TL, TO, TC, TS, TG>,
+            TO extends OnModel<T, TL, TO, TC, TS, TG>,
+            TC extends WhereModel<T, TL, TO, TC, TS, TG>,
+            TS extends SortModel<T, TL, TO, TC, TS, TG>,
+            TG extends GroupModel<T, TL, TO, TC, TS, TG>> QueryEngine<T, TL, TO, TC, TS, TG> query(String tableName, Class<T> mainClass) {
+        return new QueryEngine<>(tableName, mainClass, dataBaseType);
+    }
+
+    public <T extends Model<T, TL, TO, TC, TS, TG>,
+            TL extends ColumnModel<T, TL, TO, TC, TS, TG>,
+            TO extends OnModel<T, TL, TO, TC, TS, TG>,
+            TC extends WhereModel<T, TL, TO, TC, TS, TG>,
+            TS extends SortModel<T, TL, TO, TC, TS, TG>,
+            TG extends GroupModel<T, TL, TO, TC, TS, TG>> QueryEngine<T, TL, TO, TC, TS, TG> query(Class<T> mainClass) {
+        return query(null, mainClass);
+    }
+
+    public <T extends Model<T, TL, TO, TC, TS, TG>,
+            TL extends ColumnModel<T, TL, TO, TC, TS, TG>,
+            TO extends OnModel<T, TL, TO, TC, TS, TG>,
+            TC extends WhereModel<T, TL, TO, TC, TS, TG>,
+            TS extends SortModel<T, TL, TO, TC, TS, TG>,
+            TG extends GroupModel<T, TL, TO, TC, TS, TG>> InsertEngine<T, TL, TO, TC, TS, TG> insert(String tableName, Class<T> columnClass) {
+        return new InsertEngine<>(tableName, columnClass, dataBaseType);
+    }
+
+    public <T extends Model<T, TL, TO, TC, TS, TG>,
+            TL extends ColumnModel<T, TL, TO, TC, TS, TG>,
+            TO extends OnModel<T, TL, TO, TC, TS, TG>,
+            TC extends WhereModel<T, TL, TO, TC, TS, TG>,
+            TS extends SortModel<T, TL, TO, TC, TS, TG>,
+            TG extends GroupModel<T, TL, TO, TC, TS, TG>> InsertEngine<T, TL, TO, TC, TS, TG> insert(Class<T> columnClass) {
+        return insert(null, columnClass);
+    }
+
+    public <T extends Model<T, TL, TO, TC, TS, TG>,
+            TL extends ColumnModel<T, TL, TO, TC, TS, TG>,
+            TO extends OnModel<T, TL, TO, TC, TS, TG>,
+            TC extends WhereModel<T, TL, TO, TC, TS, TG>,
+            TS extends SortModel<T, TL, TO, TC, TS, TG>,
+            TG extends GroupModel<T, TL, TO, TC, TS, TG>> UpdateEngine<T, TL, TO, TC, TS, TG> update(String tableName, Class<T> mainClass) {
+        return new UpdateEngine<>(tableName, mainClass, dataBaseType);
+    }
+
+    public <T extends Model<T, TL, TO, TC, TS, TG>,
+            TL extends ColumnModel<T, TL, TO, TC, TS, TG>,
+            TO extends OnModel<T, TL, TO, TC, TS, TG>,
+            TC extends WhereModel<T, TL, TO, TC, TS, TG>,
+            TS extends SortModel<T, TL, TO, TC, TS, TG>,
+            TG extends GroupModel<T, TL, TO, TC, TS, TG>> UpdateEngine<T, TL, TO, TC, TS, TG> update(Class<T> mainClass) {
+        return update(null, mainClass);
+    }
+
+    public <T extends Model<T, TL, TO, TC, TS, TG>,
+            TL extends ColumnModel<T, TL, TO, TC, TS, TG>,
+            TO extends OnModel<T, TL, TO, TC, TS, TG>,
+            TC extends WhereModel<T, TL, TO, TC, TS, TG>,
+            TS extends SortModel<T, TL, TO, TC, TS, TG>,
+            TG extends GroupModel<T, TL, TO, TC, TS, TG>> DeleteEngine<T, TL, TO, TC, TS, TG> delete(String tableName, Class<T> mainClass) {
+        return new DeleteEngine<>(tableName, mainClass, dataBaseType);
+    }
+
+    public <T extends Model<T, TL, TO, TC, TS, TG>,
+            TL extends ColumnModel<T, TL, TO, TC, TS, TG>,
+            TO extends OnModel<T, TL, TO, TC, TS, TG>,
+            TC extends WhereModel<T, TL, TO, TC, TS, TG>,
+            TS extends SortModel<T, TL, TO, TC, TS, TG>,
+            TG extends GroupModel<T, TL, TO, TC, TS, TG>> DeleteEngine<T, TL, TO, TC, TS, TG> delete(Class<T> mainClass) {
+        return delete(null, mainClass);
+    }
+
+}
